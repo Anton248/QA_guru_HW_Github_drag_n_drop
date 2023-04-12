8b2f725a18a47752034baccae0e9c75f2968a44a
Anton Nikolski <ant.nikolski@gmail.com>
2023-04-12 08:39:06 +0300

    more accurate search

--- src/test/java/github_enterprise/GithubEnterpriseTests.java
+++ src/test/java/github_enterprise/GithubEnterpriseTests.java
@@ -3,6 +3,7 @@ package github_enterprise;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
@@ -17,7 +18,7 @@ public class GithubEnterpriseTests extends TestBaseGithub {
 
 // На главной странице GitHub выбрать меню Solutions -> Enterprise с помощью команды hover для Solutions.
         $("div.header-menu-wrapper").$(byText("Solutions")).hover();
-        $("a[href='/enterprise']").click();
+        $("a[href='/enterprise']").shouldHave(text("Enterprise")).click();
 
 // Грузится страница Enterprise с заголовком "Build like the best"
         $(withTagAndText("h1", "Build like the best")).should(visible);
